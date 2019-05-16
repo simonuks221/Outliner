fa82e2d9c8fe671c567cd86aa3486b7a639b3543
simonuks221 <simonasriauka@gmail.com>
2019-05-16 21:40:08 +0300

    Showing image
    Shows image and nothing else now

--- Outliner/Form1.Designer.cs
+++ Outliner/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.SensitivitySlider = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SensitivitySlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(272, 147);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // SensitivitySlider
+            // 
+            this.SensitivitySlider.Location = new System.Drawing.Point(471, 311);
+            this.SensitivitySlider.Name = "SensitivitySlider";
+            this.SensitivitySlider.Size = new System.Drawing.Size(253, 45);
+            this.SensitivitySlider.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(736, 368);
+            this.Controls.Add(this.SensitivitySlider);
+            this.Controls.Add(this.MainPictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SensitivitySlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.TrackBar SensitivitySlider;
     }
 }
 
--- Outliner/Form1.cs
+++ Outliner/Form1.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Outliner
 {
+
     public partial class Form1 : Form
     {
+        PixelInfo[,] pixelInfos;
+
+        public Image selectedImage;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader reader = new StreamReader(openFileDialog.FileName);
+
+                selectedImage = Image.FromStream(reader.BaseStream);
+                MainPictureBox.Size = selectedImage.Size;
+                MainPictureBox.Image = selectedImage;
+
+                //Setup pixel info
+                pixelInfos = new PixelInfo[selectedImage.Size.Width, selectedImage.Size.Height];
+                Bitmap bitmap = new Bitmap(selectedImage);
+                for (int x = 0; x < selectedImage.Size.Width; x++)
+                {
+                    for (int y = 0; y < selectedImage.Size.Height; y++)
+                    {
+                        PixelInfo newPixelInfo = new PixelInfo(bitmap.GetPixel(x, y), new Point(x, y));
+                        pixelInfos[x, y] = newPixelInfo;
+                    }
+                }
+
+            }
+            else
+            {
+                throw new Exception("Selected nothing or other exeption");
+            }
+        }
+    }
+
+    public class PixelInfo
+    {
+        public Color pixelColor;
+        public Point pixelLocation;
+
+        public PixelInfo(Color _pixelColor, Point _pixelLocation)
+        {
+            pixelColor = _pixelColor;
+            pixelLocation = _pixelLocation;
         }
     }
 }
